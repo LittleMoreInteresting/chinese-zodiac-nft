d5c8d2cec8fc5b526f5e0539949265966183cef0
sunhaoran <1208818417@qq.com>
2024-05-20 09:46:18 +0800

    Wallet

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,45 +1,127 @@
-"use client";
+// "use client";
 
-import React, { ReactNode } from 'react'
+// import React, { ReactNode } from 'react'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import {
-    State,
-    WagmiProvider,
-} from 'wagmi'
-import {
-    getDefaultConfig,
-    RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import {
+//     State,
+//     WagmiProvider,
+// } from 'wagmi'
+// import {
+//     getDefaultConfig,
+//     RainbowKitProvider,
+//     getDefaultWallets,
+// } from '@rainbow-me/rainbowkit';
+// import {
+//     argentWallet,
+//     trustWallet,
+//     ledgerWallet,
+//   } from '@rainbow-me/rainbowkit/wallets';
 
+// import {NextUIProvider} from "@nextui-org/react";
+// import {useRouter} from 'next/navigation'
+
+// import { wagmiConfig} from "./wagmiConfig"
+// const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string;
+// const { wallets } = getDefaultWallets();
+// export const config = getDefaultConfig({
+//     appName: "App",
+//     projectId:projectId,
+//     wallets: [
+//         ...wallets,
+//         {
+//           groupName: 'Other',
+//           wallets: [argentWallet, trustWallet, ledgerWallet],
+//         },
+//     ],
+//     ...wagmiConfig
+// })
+// // Setup queryClient
+// const queryClient = new QueryClient()
+
+// export function Providers({children,initialState}: {
+//     children: ReactNode
+//     initialState?: State
+// }) {
+//     const router = useRouter()
+
+//     return (
+//         <WagmiProvider config={config} initialState={initialState}>
+//             <QueryClientProvider client={queryClient}>
+//                 <RainbowKitProvider>
+//                     <NextUIProvider navigate={router.push}>
+//                         {children}
+//                     </NextUIProvider>
+//                 </RainbowKitProvider>
+//             </QueryClientProvider>
+//         </WagmiProvider>
+//     )
+// }
+'use client';
+
+import * as React from 'react';
 import {NextUIProvider} from "@nextui-org/react";
 import {useRouter} from 'next/navigation'
-
-import { wagmiConfig} from "./wagmiConfig"
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  getDefaultConfig,
+} from '@rainbow-me/rainbowkit';
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider,State,cookieStorage, createStorage } from 'wagmi';
+const { wallets } = getDefaultWallets();
 const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string;
-export const config = getDefaultConfig({
-    appName: "App",
-    projectId:projectId,
-    ...wagmiConfig
+const config = getDefaultConfig({
+  appName: 'CZ-NFT',
+  projectId: projectId,
+  wallets: [
+    ...wallets,
+    {
+      groupName: 'Other',
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    sepolia,
+  ],
+  ssr: true,
+  storage: createStorage({
+    key:"cz-nft",
+    storage: cookieStorage,
 })
-// Setup queryClient
-const queryClient = new QueryClient()
+});
 
-export function Providers({children,initialState}: {
-    children: ReactNode
-    initialState?: State
-}) {
+const queryClient = new QueryClient();
+
+export function Providers({ children,initialState }: { children: React.ReactNode,initialState?: State }) {
     const router = useRouter()
-
-    return (
-        <WagmiProvider config={config} initialState={initialState}>
-            <QueryClientProvider client={queryClient}>
-                <RainbowKitProvider>
-                    <NextUIProvider navigate={router.push}>
+  return (
+    <WagmiProvider config={config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+        <NextUIProvider navigate={router.push}>
                         {children}
-                    </NextUIProvider>
-                </RainbowKitProvider>
-            </QueryClientProvider>
-        </WagmiProvider>
-    )
+                    </NextUIProvider>   
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
--- frontend/src/app/wagmiConfig.ts
+++ frontend/src/app/wagmiConfig.ts
@@ -1,9 +1,13 @@
 import {cookieStorage, createConfig, createStorage, http} from "wagmi";
-import {sepolia} from "wagmi/chains";
+import {sepolia,mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base} from "wagmi/chains";
 
 const httpSepolia = process.env.NEXT_ALCHEMY_HTTP_SEPOLIA as string
 export const wagmiConfig = createConfig({
-    chains: [sepolia],
+    chains: [sepolia,mainnet, polygon, optimism, arbitrum, base],
     ssr: true,
     storage: createStorage({
         key:"nft-market",
@@ -11,6 +15,11 @@ export const wagmiConfig = createConfig({
     }),
     transports:{
         [sepolia.id]: http(httpSepolia),
+        [mainnet.id]: http(),
+        [polygon.id]: http(),
+        [optimism.id]: http(),
+        [arbitrum.id]: http(),
+        [base.id]: http(),
     },
 })
 
